b3058c77ed886c59a8b070d27255905fc1eb4836
arnav <arnav.jose@gmail.com>
2022-02-01 15:01:45 +0530

    separated code into functions

--- src/wordle/core.clj
+++ src/wordle/core.clj
@@ -3,21 +3,26 @@
   (:require [clojure.string :as strn]
             [clojure.set :refer [difference]]))
 
-(def words (strn/split-lines
-            (slurp
-             "resources/words.txt")))
+(def words "12972 member list of all possible words"
+  (strn/split-lines
+   (slurp
+    "resources/words.txt")))
 
-;; now we have a list of all possible words (12972 long)
-;; start with a word that has the most common letters and nothing repeated, like aside.
+(defn init-classes
+  "initial state that every letter is possible for all 5 letters of word"
+  []
+  (let [letters
+        (into #{} (map char (range 97 (+ 97 26))))]
+    (into [] (repeat 5 letters))))
 
-(def letters (into #{} (map char (range 97 (+ 97 26)))))
-
-;; function from [letter pos state] to new group of classes
 ;; for every class in group, have its pos, and go through prev vector by reduction.
-;; possibilities: letter state is \0 then remove from class
-;; letter state is \1 and pos is same, remove from class otherwise don't
-;; letter state is \2 and pos is same, make class the letter otherwise do nothing
-(defn update-class [ch-class class-pos letter letter-pos state]
+
+(defn update-class
+  "uses class, its position (0-4), a letter, its position and state to modify class accordingly
+  possibilities: letter state is \0 then remove from class
+  letter state is \1 and pos is same, remove from class otherwise don't
+  letter state is \2 and pos is same, make class the letter otherwise do nothing"
+  [ch-class class-pos letter letter-pos state]
   (cond (= \0 state) (difference ch-class #{letter})
         (= \1 state) (if (= class-pos letter-pos)
                        (difference ch-class #{letter})
@@ -37,33 +42,62 @@
                       result)))
        class-vector (range (count class-vector))))
 
+(defn yellow-letters
+  "uses word and encoded result from wordle to make a list of letters that are required"
+  [word result]
+  (filter (complement nil?)
+          (map (fn [letter state]
+                 (if (= \1 state) letter nil))
+               word result)))
+
+(defn validate-word
+  "Checks if  given word matches every pattern in patterns"
+  [patterns word] (every? #(re-find % word) patterns))
+
+(defn make-patterns-vector
+  "uses required letters and generated character classes (eg [a-z])
+  to make a vector of patterns for validation of words"
+  [required-letters classes]
+  (conj
+   (map #(re-pattern (str ".*" % ".*")) required-letters)
+   (re-pattern (strn/join
+                (map #(str \[ (strn/join %) \])
+                     classes)))))
+
+(defn get-first-distinct-maybe
+  "gets first word of word list with distinct letters
+  or gets the first word"
+  [word-list]
+  (or
+   (first (filter (partial apply distinct?) word-list))
+   (first word-list)))
+
 (defn -main []
-  (loop [classes (into [] (repeat 5 letters))
-         try 1
+  (loop [classes (init-classes)
+         try 2
          word-list words
-         to-print "align"]
-    
-    (cond (= try 1) (recur classes (inc try) word-list to-print)
-          (= try 7) nil
-          :else
-          (let [eff to-print
-                tried (do (println eff) (print "do you want to enter a different word? y/n ") (flush)
-                          (if (= (read-line) "y") (read-line) eff))
-                result (do (println "enter encoded result") (read-line))
-                new-classes (update-classes-with-word-and-result classes tried result)
-                required-letters (filter (complement nil?)
-                                                  (map (fn [letter state]
-                                                         (if (= \1 state) letter nil))
-                                                       tried result))
-                required-classes (map #(re-pattern (str ".*" % ".*")) required-letters)
-                new-regex-classes (map #(str \[ (strn/join %) \]) new-classes)
-                pattern (re-pattern (strn/join new-regex-classes))
-                filter-func (fn [word] (every? #(re-find % word) (conj required-classes pattern)))
-                new-word-list (filter filter-func word-list)
-                dist (first (filter (partial apply distinct?) new-word-list))
-                new-tried (or dist (first new-word-list))]
-            (cond 
-              (= result "22222") (println "success!")
-              (= nil new-tried) ((println "no words left that match :(")
-                                 (System/exit 0))
-              :else (recur new-classes (inc try) new-word-list new-tried))))))
+         to-print (do (print "enter starting word, ex. aside: ")
+                      (flush)
+                      (strn/trim (read-line)))]
+
+    (let [eff to-print
+          tried (do (printf "the chosen word is: %s\n" eff)
+                    (print "do you want to enter a different word?(y/n): ")
+                    (flush)
+                    (if (= (strn/trim (read-line)) "y")
+                      (read-line)
+                      eff))
+          result (do (print "enter encoded result from wordle: ") (flush) (read-line))
+          new-classes (update-classes-with-word-and-result classes tried result)
+          required-letters (yellow-letters tried result)
+          pattern-vector (make-patterns-vector required-letters new-classes)
+          new-word-list (filter
+                         (partial validate-word pattern-vector)
+                         word-list)
+          new-tried (get-first-distinct-maybe new-word-list)]
+
+      (cond
+        (= result "22222") (println "success!")
+        (= nil new-tried) ((println "no words left that match :(")
+                           (System/exit 0))
+        :else (recur new-classes (inc try) new-word-list new-tried)))))
