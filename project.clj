efec9fd72183125bac92dacfc638637f8b939362
arnav <arnav.jose@gmail.com>
2022-02-01 15:45:56 +0530

    README and other things

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject wordle "0.9.0"
+  :description "A program to solve wordle puzzles"
+  :url "http://github.com/crmsnbleyd/wordle"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.3"]]
+  :main ^:skip-aot wordle.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
